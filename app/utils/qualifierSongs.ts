fbb7a25eca49a42d2386acd30fee7b5709af4167
Albert Shin <albshin07@gmail.com>
2023-10-03 02:45:49 -0400

    add qualifier songs

--- app/utils/qualifierSongs.ts
+++ app/utils/qualifierSongs.ts
@@ -1,20 +1,34 @@
-import aSiteDeLaRue from '../routes/assets/asitedelarue.png';
+import district1 from '../routes/assets/district1.png';
+import blackswan from '../routes/assets/blackswan.png';
+import heartrabbitcoaster from '../routes/assets/heartrabbitcoaster.png';
+import nihilism from '../routes/assets/nihilism.png';
+import asitedelarue from '../routes/assets/asitedelarue.png';
 import sarabande from '../routes/assets/sarabande.png';
 import skeptic from '../routes/assets/skeptic.png';
+import achluoias from '../routes/assets/achluoias.png';
 
 const qualifierSongs = [
-  { name: 'District 1', difficulty: 'S10' },
-  { name: 'Black Swan', difficulty: 'S12' },
-  { name: 'Heart Rabbit Coaster', difficulty: 'S14' },
+  { name: 'District 1', difficulty: 'S10', image: district1 },
+  { name: 'Black Swan', difficulty: 'S12', image: blackswan },
+  {
+    name: 'Heart Rabbit Coaster',
+    difficulty: 'S14',
+    image: heartrabbitcoaster,
+  },
   {
     name: 'A Site De La Rue',
     difficulty: 'S16',
-    image: aSiteDeLaRue,
+    image: asitedelarue,
   },
-  { name: 'Nihilism', difficulty: 'S18' },
+  { name: 'Nihilism -Another Ver.-', difficulty: 'S18', image: nihilism },
   { name: 'Sarabande', difficulty: 'S20', image: sarabande },
   { name: 'Skeptic', difficulty: 'S22', image: skeptic },
-  { name: 'Vanish', difficulty: 'D24', border: 'green.400' },
+  {
+    name: 'Achluoias',
+    difficulty: 'D24',
+    border: 'green.400',
+    image: achluoias,
+  },
 ];
 
 export { qualifierSongs };
